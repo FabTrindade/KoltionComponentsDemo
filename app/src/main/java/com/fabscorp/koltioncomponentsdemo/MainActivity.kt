a70793bd71adf3cce95636c9ead01b69ae9bac68
Fabricio Trindade <fabrtrindade@gmail.com>
2024-05-31 17:16:40 -0300

    Toast

--- app/src/main/java/com/fabscorp/koltioncomponentsdemo/MainActivity.kt
+++ app/src/main/java/com/fabscorp/koltioncomponentsdemo/MainActivity.kt
@@ -2,10 +2,29 @@ package com.fabscorp.koltioncomponentsdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.fabscorp.koltioncomponentsdemo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var bindding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindding.root)
+
+        supportActionBar?.hide()
+
+        bindding.buttonToast.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            bindding.buttonToast.id -> {
+                val toast = Toast.makeText(this, "Toast me!", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
     }
 }
